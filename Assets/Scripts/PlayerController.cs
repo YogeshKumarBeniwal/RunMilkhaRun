1221595134d4054b5bc60a001c0b35fa63f1514b
Yogesh Kumar <yogeshkumarbeniwal1999@gmail.com>
2020-04-17 17:05:04 +0530

    Added project files and implemented player jump.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForse = 10f;
+    public float gravityMultiplier = 1;
+
+    private Rigidbody playerRB;
+    private bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            isOnGround = false;
+            playerRB.AddForce(Vector3.up * jumpForse, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+
+}
